2225371ed5436a52e96aea631cdbe7bdd507361f
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-02-17 13:48:22 +0530

    creating billing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { BillingComponent } from './billing/billing.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     NavbarComponent,
     HomeComponent,
     SidebarComponent,
+    BillingComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/billing/billing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-billing',
+  templateUrl: './billing.component.html',
+  styleUrls: ['./billing.component.css']
+})
+export class BillingComponent {
+
+}
