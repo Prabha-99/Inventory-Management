f72c3ba94d17657b8dd270a5cf5fc99b13c74a2e
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-02-24 02:06:29 +0530

    bill ts

--- src/app/billing/billing.component.ts
+++ src/app/billing/billing.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-billing',
   templateUrl: './billing.component.html',
   styleUrls: ['./billing.component.css']
+
 })
 
 export class BillingComponent {
 
 }
-
 
