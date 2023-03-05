a08afc811d540c1d33b8a5da7b1bbca87fd455b2
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-03-05 12:23:32 +0530

    update ts 23/5/3

--- src/app/billing/billing.component.ts
+++ src/app/billing/billing.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-
+import * as jsPDF from 'jspdf';
 
 @Component({
+  
   selector: 'app-billing',
   templateUrl: './billing.component.html',
   styleUrls: ['./billing.component.css']
@@ -9,28 +10,49 @@ import { Component } from '@angular/core';
 })
 
 export class BillingComponent {
-    addRow(): void {
+
+  calno():void{
+    const table = document.getElementById("tobody") as HTMLTableElement; 
+    for (var i = 1; i < table.rows.length; i++) { // iterate over rows
+      const rown = table.rows[i];
+      rown.cells[0].textContent = (i+1).toString(); 
+    }
+      }
+
+
+  addRow(): void {
+    const row = document.createElement('tr'); 
+
+    const col = document.createElement('td'); 
+    const col2 = document.createElement('td'); 
+    const col3 = document.createElement('td'); 
+    const col4 = document.createElement('td'); 
+    const col5 = document.createElement('td'); 
+    const col6 = document.createElement('td');
      
- const table = document.getElementById("itemTable") as HTMLTableElement;
- const row = table.insertRow(2);
- const cell1 = row.insertCell(0);
- const cell2 = row.insertCell(1);
- const cell3 = row.insertCell(2);
- const cell4 = row.insertCell(3);
- const cell5 = row.insertCell(4);
- const cell6 = row.insertCell(5);
+    row.appendChild(col);
+    row.appendChild(col2);
+    row.appendChild(col3);
+    row.appendChild(col4);
+    row.appendChild(col5); 
+    row.appendChild(col6); 
+   
+     
+    col.innerHTML=''+this.calno();
+    col2.innerHTML = '<select id="Select the product"><option id="Melamaine">Melamaine</option><option id="H.Band">H-Band</option><option id="M1">material 1</option><option id="M2">material 2</option></select>' ; 
+    col3.innerHTML = '<input type="text"maxlength="4"size="4"id="qty">'; 
+    col4.innerHTML="$$";
+    col5.innerHTML = '<input type="text"maxlength="4"size="4"id="qty">'; 
+    col6.innerHTML = "$$"; 
 
- var i=1;
- i+=1;
- cell1.innerHTML = ""+i;
- 
- cell2.innerHTML = "NEW CELL2";
- cell3.innerHTML = "NEW CELL3";
- cell4.innerHTML = "NEW CELL4";
- cell5.innerHTML = "NEW CELL5";
- cell6.innerHTML = "NEW CELL6";
+   const table = document.getElementById("tobody");
+    table!.appendChild(row); 
 
-}
+
+
+  }
+
+
 }
 
 
