9e3e03f38ed6ca71c89292821c7de92f8de727c2
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-03-11 13:53:17 +0530

    send component ts

--- src/app/billing-send/billing-send.component.ts
+++ src/app/billing-send/billing-send.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-billing-send',
   templateUrl: './billing-send.component.html',
-  styleUrls: ['./billing-send.component.css']
+  styleUrls: ['./billing-send.component.css'],
+  
+
 })
+
+
 export class BillingSendComponent {
 
+  title = 'EmailTemplate';
+
+  dataset: Details = {
+    name:'',
+    age:0,
+    country:'',
+    email:''
+  };
+
+  constructor(private https: HttpClient){ }
+
+  onSubmit()
+  {
+    this.https.post<Details>('http://localhost:8080/testapp/getdetails', this.dataset).subscribe(
+      res => {
+        this.dataset = res;
+        console.log(this.dataset);
+        alert('Email Sent successfully');
+        this.dataset.age = 0;
+        this.dataset.name = '';
+        this.dataset.country = '';
+        this.dataset.email = '';
+        
+      });
+  }
 }
+
+interface Details
+{
+  name:string;
+  age:number;
+  country:string;
+  email:string;
+}
+
+
