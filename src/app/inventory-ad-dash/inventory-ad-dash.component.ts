562625ae8808dc442a61a7f652bbc7ca68c6b9fc
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-04-06 20:11:47 +0530

    updated

--- src/app/inventory-ad-dash/inventory-ad-dash.component.ts
+++ src/app/inventory-ad-dash/inventory-ad-dash.component.ts
@@ -1,24 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
-import { count } from 'rxjs';
+// import { HttpClient } from '@angular/common/http'; 
+// import { count } from 'rxjs';
 
 
 
 @Component({
   selector: 'app-inventory-ad-dash',
   templateUrl: './inventory-ad-dash.component.html',
-  styleUrls: ['./inventory-ad-dash.component.css']
-})
-export class InventoryAdDashComponent implements OnInit {
-  productCount!: number;
-
+  styleUrls: ['./inventory-ad-dash.component.css'],
   
-  constructor(private http: HttpClient){}
-
-  ngOnInit(): void {
-      this.http.get<number>('http://localhost:8080/api/v1/product/productCount').subscribe(count =>{
-        console.log('Product count:',count);
-        this.productCount = count;
-      });
-  }
+})
+export class InventoryAdDashComponent {
+  
+  
 }
