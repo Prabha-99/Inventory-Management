7cdbf21f24d295a039348bea7a4dabe444db19c8
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-05-01 13:11:23 +0530

    update stock manager

---
+++ src/app/stock-manager-nav/stock-manager-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-manager-nav',
+  templateUrl: './stock-manager-nav.component.html',
+  styleUrls: ['./stock-manager-nav.component.css']
+})
+export class StockManagerNavComponent {
+
+}
---
+++ src/app/stock-manager-product/stock-manager-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-manager-product',
+  templateUrl: './stock-manager-product.component.html',
+  styleUrls: ['./stock-manager-product.component.css']
+})
+export class StockManagerProductComponent {
+
+}
