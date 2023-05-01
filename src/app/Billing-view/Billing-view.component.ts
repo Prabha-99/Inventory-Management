2f65932cc4b935fc5189ade78798ea38613da3aa
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-05-01 14:33:48 +0530

    ts update billing

--- src/app/Billing-view/Billing-view.component.ts
+++ src/app/Billing-view/Billing-view.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Observable } from 'rxjs/internal/Observable';
+import { BillService } from './billing-view.component.service';
 
 @Component({
   selector: 'app-billing-view',
@@ -8,5 +12,5 @@ import { Component } from '@angular/core';
 export class BillingViewComponent {
 
 
-  
+
 }
