052fa31ec409a9f2c7286a794e0832fae99d7f0a
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-05-07 15:13:31 +0530

    Rerouted the Login Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  {path:"",redirectTo:"/login",pathMatch:"full"},
+  {path:"",component:LoginComponent},
   {path:"home",component:HomeComponent},
   {path:"navbar",component:NavbarComponent},
   {path:"sidebar",component:SidebarComponent},
