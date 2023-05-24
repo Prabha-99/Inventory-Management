5c9e5193419fc7937b43255a829ae32e9ac4bac1
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-05-25 01:00:50 +0530

    adding products

--- src/app/stock-manager-product-popup/stock-manager-product-popup.component.ts
+++ src/app/stock-manager-product-popup/stock-manager-product-popup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 
 @Component({
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./stock-manager-product-popup.component.css']
 })
 export class StockManagerProductPopupComponent {
+
+  selectedCategory: string="";
+  productName: string="";
+  productBrand: string="";
+  productPrice: number= 0;
+  productQuantity: number= 0;
+
+  constructor(private http: HttpClient) { }
+
+  saveProduct() {
+    const product = {
+      category: this.selectedCategory,
+      productName: this.productName,
+      productBrand: this.productBrand,
+      productPrice: this.productPrice,
+      productQuantity: this.productQuantity
+    };
+
+    this.http.post<any>('/api/product/saveProduct', product).subscribe(
+      (response) => {
+        console.log('Product saved successfully:', response);
+        // Handle success scenario
+      },
+      (error) => {
+        console.error('Error occurred while saving product:', error);
+        // Handle error scenario
+      }
+    );
+  }
+
+  
+
 }
