970a8c0a5a82d6dcfbe75ff6b4da9e8386041a33
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-05-27 10:26:26 +0530

    Imported some Angular material moduls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,13 @@ import {MatInputModule} from  '@angular/material/input';
 import { MatOptionModule } from '@angular/material/core';
 import {MatSelectModule} from '@angular/material/select';
 import { StockManagerProductPopupComponent } from './stock-manager-product-popup/stock-manager-product-popup.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
+
 
 
 @NgModule({
