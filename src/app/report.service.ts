c43ac3f254a6267ca4c83214925cc1d7c6503b7d
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-05-28 22:19:43 +0530

    Added a new Service for report download option

---
+++ src/app/report.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportService {
+
+  private apiUrl = 'http://localhost:8080/api/reports/all';
+
+  constructor(private http: HttpClient) { }
+
+  getFiles(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}`);
+  }
+
+  downloadFile(fileId: number): Observable<Blob> {
+    return this.http.get(`${this.apiUrl}/files/${fileId}`, { responseType: 'blob' });
+  }
+  
+}
