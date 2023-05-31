521361b4d83289e070fd9ae86ef45db3ad9f82ed
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-05-31 09:47:11 +0530

    Connected some Endpoint to get the counts of couple of Elements

---
+++ src/app/dash.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashService {
+
+  private apiUrl = '/api/cards';
+
+  constructor(private http: HttpClient) { }
+
+}
