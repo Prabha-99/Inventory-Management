5349ca9284a3eaef3e0f4b9a4e63488e410b77b9
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-05-31 10:43:42 +0530

    Home Componenet Update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,9 +16,9 @@ export class HomeComponent implements OnInit{
   constructor(private http:HttpClient){}
   
   ngOnInit() {
-    this.http.get<any>("http://localhost:8080/api/product/productCount").subscribe(count =>this.category = count);
+    this.http.get<any>("http://localhost:8080/api/v1/category/categoryCount").subscribe(count =>this.category = count);
     this.http.get<any>("http://localhost:8080/api/product/productCount").subscribe(count =>this.products = count);
-    this.http.get<any>("http://localhost:8080/api/product/productCount").subscribe(count =>this.sales = count);
+    this.http.get<any>("").subscribe(count =>this.sales = count);
     
   }
 }
