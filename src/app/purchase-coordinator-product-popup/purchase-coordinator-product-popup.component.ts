4b1e6a53b0dbcc88e2f01c1ab218b802cf0a8112
hasaniam <102344424+hasaniam@users.noreply.github.com>
2023-06-19 15:29:03 +0530

    purchase-coordinator-popup-service

--- src/app/purchase-coordinator-product-popup/purchase-coordinator-product-popup.component.ts
+++ src/app/purchase-coordinator-product-popup/purchase-coordinator-product-popup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PurchaseCoordinatorProductPopupService } from '../purchase-coordinator-product-popup.service';
 
 @Component({
   selector: 'app-purchase-coordinator-product-popup',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./purchase-coordinator-product-popup.component.css']
 })
 export class PurchaseCoordinatorProductPopupComponent {
+  product: any = {
 
+    category_id: '',
+    product_name: '',
+    product_brand: '',
+    product_quantity: null,
+    product_price: null
+  };
+
+  constructor(private PurchaseCoordinatorProductPopupService:PurchaseCoordinatorProductPopupService) {}
+
+  addProduct() {
+    this.PurchaseCoordinatorProductPopupService.addProduct(this.product).subscribe(
+      (response) => {
+        console.log('Product added successfully');
+        // Reset the form
+        this.resetForm();
+      },
+      (error) => {
+        console.error('Failed to add product:', error);
+      }
+    );
+  }
+
+  resetForm() {
+    this.product = {
+      category_id: '',
+      product_name: '',
+      product_brand: '',
+      product_quantity: null,
+      product_price: null
+    };
+  }
 }
