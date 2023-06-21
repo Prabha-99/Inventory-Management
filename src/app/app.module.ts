efd55aa73a06c78e6404c905bf86cd9e4a04d476
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-06-22 01:14:48 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -74,6 +74,7 @@ import { StockKeeperOrderReportsGRNComponent } from './stock-keeper-order-report
 // import { StockKeeperOrderReportsComponentComponent } from './stock-keeper-order-reports-component/stock-keeper-order-reports-component.component';
 
 import { PurchaseCoordinatorEditComponent } from './purchase-coordinator-edit/purchase-coordinator-edit.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
 
 
 
@@ -143,6 +144,7 @@ import { PurchaseCoordinatorEditComponent } from './purchase-coordinator-edit/pu
 
     DeleteUserComponent,
       PurchaseCoordinatorEditComponent,
+      DeleteProductComponent,
 
   
 
--- src/app/inventory-ad-addproduct/inventory-ad-addproduct.service.ts
+++ src/app/inventory-ad-addproduct/inventory-ad-addproduct.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryAdAddproductService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public dialog: MatDialog) { }
 
   getProducts() {
     return this.http.get<any[]>('http://localhost:8080/api/product/getAllProduct'); //get products
