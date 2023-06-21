c884de758f09e8b2dcaa4e9b9fb22fc4655aa8de
Nimesha Sewwandi <103416158+nimeshasewwandi@users.noreply.github.com>
2023-06-21 16:52:25 +0530

    st kp sidebar

---
+++ src/app/stock-keeper-sidebar/stock-keeper-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-keeper-sidebar',
+  templateUrl: './stock-keeper-sidebar.component.html',
+  styleUrls: ['./stock-keeper-sidebar.component.css']
+})
+export class StockKeeperSidebarComponent {
+
+}
