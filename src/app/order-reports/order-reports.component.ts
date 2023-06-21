2a41f8d5c2853ef14adc56a7eada6701575d58c3
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-06-21 09:07:56 +0530

    Implemented getFiles and downloadFiles methods in Component

--- src/app/order-reports/order-reports.component.ts
+++ src/app/order-reports/order-reports.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReportService } from './report.service';
 
 @Component({
   selector: 'app-order-reports',
   templateUrl: './order-reports.component.html',
   styleUrls: ['./order-reports.component.css']
 })
-export class OrderReportsComponent {
+export class OrderReportsComponent implements OnInit{
+
+  files!: any[];
+
+  constructor(private reportService: ReportService) { }
+
+  ngOnInit(): void {
+    this.reportService.getAllFiles().subscribe(
+      (response) => {
+        this.files = response;
+      },
+      (error) => {
+        console.log('Error retrieving files:', error);
+      }
+    );
+  }
+
+  downloadFile(report_id: number, report_name: string): void {
+    this.reportService.downloadFile(report_id).subscribe(
+      (response) => {
+        // Create a temporary link and trigger the file download
+        const blob = new Blob([response], { type: 'application/pdf' });
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = report_name;
+        link.click();
+      },
+      (error) => {
+        console.log('Error downloading file:', error);
+      }
+    );
+  }
+ 
 
 }
