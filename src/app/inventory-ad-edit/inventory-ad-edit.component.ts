1d1398a643ab4764a79942fe4a047a9bffbd597d
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-06-27 19:36:17 +0530

    updated

--- src/app/inventory-ad-edit/inventory-ad-edit.component.ts
+++ src/app/inventory-ad-edit/inventory-ad-edit.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InventoryAdEditService } from './inventory-ad-edit.service';
 
 @Component({
   selector: 'app-inventory-ad-edit',
   templateUrl: './inventory-ad-edit.component.html',
   styleUrls: ['./inventory-ad-edit.component.css']
 })
-export class InventoryAdEditComponent {
+export class InventoryAdEditComponent implements OnInit{
 
+  product: Product = new Product();
+
+  constructor(private inventoryAdEditService : InventoryAdEditService) {}
+  ngOnInit(): void {}
+
+  openEditProductForm(product: Product) {
+    // Assign the selected product to the component's product property
+    this.product = Object.assign({}, product);
+  }
+
+  updateProduct() {
+    // Call the service method to update the product
+    this.inventoryAdEditService.updateProduct(this.product.product_id, this.product)
+      .subscribe(
+        updatedProduct => {
+          // Handle the response or perform any additional actions
+          console.log('Product updated successfully:', updatedProduct);
+        },
+        error => {
+          // Handle error responses
+          console.error('Error updating product:', error);
+        }
+      );
+  }
+
+}
+
+export class Product {
+  product_id: any;
+  category_id: any;
+  product_brand: any;
+  product_name: any;
+  product_price: any;
+  product_quantity: any;
 }
--- src/app/inventory-ad-edit/inventory-ad-edit.service.ts
+++ src/app/inventory-ad-edit/inventory-ad-edit.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './inventory-ad-edit.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryAdEditService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/product';
+
+  constructor(private http: HttpClient) { }
+
+  updateProduct(productID: number, product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/updateProduct/${productID}`;
+    return this.http.put<Product>(url, product);
+  }
 }
