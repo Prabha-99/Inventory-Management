80fb9e015aa5afcb41aa029e42a2f0e7ebd229c2
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-06-30 17:16:05 +0530

    updated

--- src/app/stock-manager-purchase-order/stock-manager-purchase-order.component.ts
+++ src/app/stock-manager-purchase-order/stock-manager-purchase-order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StockManagerPurchaseOrderService } from './stock-manager-purchase-order.service';
 
 @Component({
   selector: 'app-stock-manager-purchase-order',
@@ -7,24 +8,19 @@ import { Component } from '@angular/core';
 })
 export class StockManagerPurchaseOrderComponent {
 
-  product: any = {
-
-    product_name: '',
-    product_brand: '',
-    product_quantity: null
-  };
-
-  increaseQuantity() {
-
-    if(!this.isRequired()){
-      alert("Pleace fill all Fields!!");
-      return;
-    }
-
+  constructor(private productService: StockManagerPurchaseOrderService) {}
+  
+  reduceQuantity(product_name: string, product_brand: string, product_quantity: number): void {
+    this.productService.reduceProductQuantity(product_name, product_brand, product_quantity).subscribe(() => {
+      alert('Product quantity updated successfully');
+    }, error => {
+      alert('Error updating product quantity');
+      console.error(error);
+    });
   }
 
-  isRequired(): boolean{
-    return !!this.product.product_name && !!this.product.product_brand && !!this.product.quantity;
+  onSubmit(formValue: any): void {
+    this.reduceQuantity(formValue.product_name, formValue.product_brand, formValue.product_quantity);
   }
 
 }
