bf5efbc410a15f434279f7a71cefceab1ebcb5d0
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-06-30 17:02:24 +0530

    updated

--- src/app/inventory-ad-purchase-order/inventory-ad-purchase-order.component.ts
+++ src/app/inventory-ad-purchase-order/inventory-ad-purchase-order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InventoryAdPurchaseOrderService } from './inventory-ad-purchase-order.service';
 
 @Component({
   selector: 'app-inventory-ad-purchase-order',
@@ -6,23 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./inventory-ad-purchase-order.component.css']
 })
 export class InventoryAdPurchaseOrderComponent {
-  product: any = {
-
-    product_name: '',
-    product_brand: '',
-    product_quantity: null
-  };
-
-  increaseQuantity() {
-
-    if(!this.isRequired()){
-      alert("Pleace fill all Fields!!");
-      return;
-    }
-
+  
+  constructor(private productService: InventoryAdPurchaseOrderService) {}
+  
+  reduceQuantity(product_name: string, product_brand: string, product_quantity: number): void {
+    this.productService.reduceProductQuantity(product_name, product_brand, product_quantity).subscribe(() => {
+      alert('Product quantity updated successfully');
+    }, error => {
+      alert('Error updating product quantity');
+      console.error(error);
+    });
   }
 
-  isRequired(): boolean{
-    return !!this.product.product_name && !!this.product.product_brand && !!this.product.quantity;
+  onSubmit(formValue: any): void {
+    this.reduceQuantity(formValue.product_name, formValue.product_brand, formValue.product_quantity);
   }
+
+
 }
