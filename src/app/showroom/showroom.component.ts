d461d99a102e5701d2ae6b25f7184320e6865243
PrasadJT <thilina1091@gmail.com>
2023-07-05 03:37:31 +0530

    create bill view

--- src/app/showroom/showroom.component.ts
+++ src/app/showroom/showroom.component.ts
@@ -1,15 +1,34 @@
-import { HttpClient } from '@angular/common/http';
-import { Component , } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit , } from '@angular/core';
+
+interface DesignerBillSend {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-showroom',
   templateUrl: './showroom.component.html',
   styleUrls: ['./showroom.component.css']
 })
-export class ShowroomComponent {
+export class ShowroomComponent implements OnInit {
 name:string = "";
 file:any;
+  showFileList: boolean | undefined;
   constructor(private http:HttpClient) {}
+
+  ngOnInit() {
+    this.getFiles();
+  }
+
+  toggleFileList() {
+    this.showFileList = !this.showFileList;
+  }
+
+  getFiles() {
+    this.http.get<DesignerBillSend[]>('http://localhost:8080/api/showroom/viewBill')
+      .subscribe(files => this.file = files);
+  }
 
   getName(name:string){
     this.name = name;
@@ -17,7 +36,6 @@ file:any;
 
   getFile(event:any){
     this.file=event.target.files[0];
-    console.log("file",this.file);
   }
 
   submitData(){
@@ -35,6 +53,22 @@ file:any;
 
   });
  }
+
+ downloadFile(id: number,fileName: string) {
+  // Your existing downloadFile method implementation
+  const fileId = 30; // Replace with the actual file ID
+
+  this.http.get('http://localhost:8080/api/showroom/viewBill' + id, {
+    responseType: 'arraybuffer',
+    headers: new HttpHeaders().append('Content-Type', 'application/json')
+  }).subscribe((response: ArrayBuffer) => {
+    const blob = new Blob([response], { type: 'application/octet-stream' });
+    const downloadLink = document.createElement('a');
+    downloadLink.href = window.URL.createObjectURL(blob);
+    downloadLink.download = fileName; // Replace with the actual filename
+    downloadLink.click();
+  });
+}
   }
 
 
