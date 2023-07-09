f9258b801c577edc5a88777d93997aba6e6519b4
PrasadJT <thilina1091@gmail.com>
2023-07-09 18:17:17 +0530

    create bill view

--- src/app/showroom/showroom.component.ts
+++ src/app/showroom/showroom.component.ts
@@ -1,15 +1,36 @@
-import { HttpClient } from '@angular/common/http';
-import { Component , } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit , } from '@angular/core';
+
+
+
+interface DesignerBillSend {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-showroom',
   templateUrl: './showroom.component.html',
   styleUrls: ['./showroom.component.css']
 })
-export class ShowroomComponent {
+export class ShowroomComponent implements OnInit {
 name:string = "";
 file:any;
+
+showFileList: boolean = false;
+showFileUpload: boolean = false;
+
+files: any;
   constructor(private http:HttpClient) {}
+
+  ngOnInit() {
+    this.getFiles();
+  }
+
+  getFiles() {
+    this.http.get<DesignerBillSend[]>('http://localhost:8080/api/showroom/viewBill')
+      .subscribe(files => this.files = files);
+  }
 
   getName(name:string){
     this.name = name;
@@ -18,6 +39,18 @@ file:any;
   getFile(event:any){
     this.file=event.target.files[0];
     console.log("file",this.file);
+  }
+
+  toggleFileList() {
+    this.showFileList = !this.showFileList;
+    this.showFileUpload = false;
+
+  }
+
+  toggleFileUpload() {
+    this.showFileUpload = !this.showFileUpload;
+    this.showFileList = false;
+
   }
 
   submitData(){
@@ -35,6 +68,36 @@ file:any;
 
   });
  }
+
+ downloadFile(id: number, fileName: string) {
+
+  const httpOptions = {
+    responseType: 'blob' as 'json',
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  // Send a GET request to the backend endpoint
+  this.http.get('http://localhost:8080/api/showroom/download?id=' + id, httpOptions)
+  .subscribe((response: any) => {
+
+      // Create a blob from the response data
+      const blob = new Blob([response], { type: 'application/octet-stream' });
+
+      // Create a temporary URL for the blob
+      const url = URL.createObjectURL(blob);
+
+      // Trigger the file download
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = fileName;
+      link.click();
+    }, error => {
+      console.error('Error occurred while downloading the file:', error);
+    });
+}
+
   }
 
 
