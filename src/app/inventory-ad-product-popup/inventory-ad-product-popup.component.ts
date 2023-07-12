4284b860b26d064ffa9d389ee48df51ec208e9ed
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-07-12 17:35:52 +0530

    update

--- src/app/inventory-ad-product-popup/inventory-ad-product-popup.component.ts
+++ src/app/inventory-ad-product-popup/inventory-ad-product-popup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InventoryAdProductPopupService } from './inventory-ad-product-popup.service';
 
 @Component({
@@ -6,7 +6,8 @@ import { InventoryAdProductPopupService } from './inventory-ad-product-popup.ser
   templateUrl: './inventory-ad-product-popup.component.html',
   styleUrls: ['./inventory-ad-product-popup.component.css']
 })
-export class InventoryAdProductPopupComponent {
+export class InventoryAdProductPopupComponent implements OnInit{
+  categories: any[] = [];
   product: any = {
 
     category_id: '',
@@ -19,6 +20,12 @@ export class InventoryAdProductPopupComponent {
   disableQuantityField = true;
 
   constructor(private inventoryAdProductPopupService: InventoryAdProductPopupService) {}
+  
+  ngOnInit(): void {
+    this.inventoryAdProductPopupService.getCategory().subscribe(category => {
+      this.categories = category;
+  });
+  }
 
   updateQuantityField() {
     this.disableQuantityField = this.product.category_id !== "c1";
