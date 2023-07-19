32f6ef60341c37fdf8a27a4d7082bb929bf0b51e
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-07-20 01:04:44 +0530

    updated

--- src/app/inventory-backup/inventory-backup.component.ts
+++ src/app/inventory-backup/inventory-backup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { InventoryBackupService } from './inventory-backup.service';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-inventory-backup',
@@ -7,13 +8,29 @@ import { InventoryBackupService } from './inventory-backup.service';
   styleUrls: ['./inventory-backup.component.css']
 })
 export class InventoryBackupComponent {
+  showProgress: boolean = false;
+  progressValue: number = 0;
 
   constructor(private backupService: InventoryBackupService) {}
 
   createBackup() {
+    this.showProgress = true;
+    this.progressValue = 0;
+
     this.backupService.createBackup().subscribe(
-      response => console.log(response),
-      error => console.error(error)
+      (event: any) => {
+        if (event.type === HttpEventType.UploadProgress) {
+          // Now TypeScript knows the type of the 'event' object
+          this.progressValue = Math.round((100 * event.loaded) / event.total);
+        } else if (event instanceof HttpResponse) {
+          console.log(event.body); // This will log the response after the backup is complete
+          this.showProgress = false;
+        }
+      },
+      error => {
+        console.error(error);
+        this.showProgress = false;
+      }
     );
   }
 }
