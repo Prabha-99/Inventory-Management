83eb5bc3b590e7aa46191a4f5c8a93f0ee896f04
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-07-21 18:56:16 +0530

    updated

--- src/app/purchase-coordinator-purchase-order/purchase-coordinator-purchase-order.component.ts
+++ src/app/purchase-coordinator-purchase-order/purchase-coordinator-purchase-order.component.ts
@@ -54,8 +54,8 @@ export class PurchaseCoordinatorPurchaseOrderComponent implements OnInit{
     });
   }
   
-  reduceQuantity(product_name: string, product_brand: string, product_quantity: number): void {
-    this.productService.reduceProductQuantity(product_name, product_brand, product_quantity).subscribe(() => {
+  increaseQuantity(product_name: string, product_brand: string, product_quantity: number): void {
+    this.productService.increaseProductQuantity(product_name, product_brand, product_quantity).subscribe(() => {
       alert('Product quantity updated successfully');
     }, error => {
       alert('Error updating product quantity');
@@ -64,7 +64,7 @@ export class PurchaseCoordinatorPurchaseOrderComponent implements OnInit{
   }
 
   onSubmit(formValue: any): void {
-    this.reduceQuantity(formValue.product_name, formValue.product_brand, formValue.product_quantity);
+    this.increaseQuantity(formValue.product_name, formValue.product_brand, formValue.product_quantity);
   }
 
   searchProductNames(): void {
