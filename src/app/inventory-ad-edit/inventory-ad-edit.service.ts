50f674947368909ba7516f0f5e8128db62d478e9
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-07-26 01:41:05 +0530

    update

--- src/app/inventory-ad-edit/inventory-ad-edit.service.ts
+++ src/app/inventory-ad-edit/inventory-ad-edit.service.ts
@@ -1,14 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryAdEditService {
 
+  private getUrl = 'http://localhost:8080/api/product';
+
   constructor(private http: HttpClient) { }
 
    getCategory() {
     return this.http.get<any[]>('http://localhost:8080/api/v1/category/getAllCategory'); //get categories
+  }
+
+  getProduct(product_id: number): Observable<any> {
+    const url = `${this.getUrl}/${product_id}`;
+    return this.http.get<any>(url);
+  }
+
+  updateProduct(product_id: number, product: any): Observable<any> {
+    const url = `${this.getUrl}/${product_id}`;
+    return this.http.put<any>(url, product);
   }
 }
